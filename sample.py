9a49c293898af7f005002f40de7954edf7ec1c8a
Damon-Shen <263018257@qq.com>
2015-07-09 15:32:36 +0800

    简单的例子

--- sample.py
+++ sample.py
@@ -4,16 +4,16 @@ import BaiduVoiceTranslationAPI
 import RecordAPI
 import sys
  
-apiKey = ""                             #这里输入自己的key
-secretKey = ""                          #这里输入自己的secretKey
-audioFile = sys.path[0] + '/Translation.wav'  				#生成并解析的语音路径+文件名 默认在当前程序所在目录
-Language = "zh"                         			        #默认zh 支持中文(zh) 英文(en) 粤语(ct)                         					 		
-Rate = 16000                          				        #采样率，支持8000和16000
-Channel = 1                             			        #声道，目前baidu只支持单声道
-Format = 'wav'                                              #语音格式，支持wav pcm opus speex amr x-flac
-Chunk = 1024                                                #录音块长度
-Record_secounds = 5                                         #录音时长，单位:秒
-Input_device = 1
+apiKey = ""                                            #这里输入自己的key
+secretKey = ""                                         #这里输入自己的secretKey
+audioFile = sys.path[0] + '/Translation.wav'  				     #生成并解析的语音路径+文件名 默认在当前程序所在目录
+Language = "zh"                         			            #默认zh 支持中文(zh) 英文(en) 粤语(ct)                         					 		
+Rate = 16000                          				             #采样率，支持8000和16000
+Channel = 1                             			            #声道，目前baidu只支持单声道
+Format = 'wav'                                         #语音格式，支持wav pcm opus speex amr x-flac
+Chunk = 1024                                           #录音块长度
+Record_secounds = 5                                    #录音时长，单位:秒
+Input_device = 1                                       #输入设备id
 
 
 if (not apiKey or not secretKey):
